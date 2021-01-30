6f51d9b347c56604df74e36241227fc41ffe06f0
Chris <>
2021-01-30 17:04:33 -0500

    Update search.js

--- static/plugins/search/search.js
+++ static/plugins/search/search.js
@@ -11,8 +11,8 @@ var fuseOptions = {
   keys: [
     {name:"title",weight:0.8},
     {name:"contents",weight:0.5},
-    {name:"tags",weight:0.3},
-    {name:"categories",weight:0.3}
+    {name:"",weight:0.3},
+    {name:"",weight:0.3}
   ]
 };
 
